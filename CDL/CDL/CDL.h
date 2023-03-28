bf02e7e061456ad32a07431ae28f0cf506ef4d88
HuberWong <85234311+HuberWong@users.noreply.github.com>
2023-03-29 07:29:23 +0800

    add  framework

---
+++ CDL/CDL/CDL.h
@@ -0,0 +1,18 @@
+//
+//  CDL.h
+//  CDL
+//
+//  Created by huber wang on 2023/3/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CDL.
+FOUNDATION_EXPORT double CDLVersionNumber;
+
+//! Project version string for CDL.
+FOUNDATION_EXPORT const unsigned char CDLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CDL/PublicHeader.h>
+
+
